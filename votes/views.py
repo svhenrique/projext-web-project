e0a47cb306ebaf05497e74d67a3b9b79e58278ed
svhenrique <silviohenrique1234@hotmail.com>
2021-06-04 17:42:24 -0300

    index view adiciondo

--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import FormView, TemplateView
+from .models import CamaraVoto
 
-# Create your views here.
+class IndexView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+
+        camaras = CamaraVoto.objects.all()
+        camaras_data = []
+
+        for camara in camaras:
+
+            nome = camara.nome
+            ativo = camara.ativo
+            inicio = camara.inicio
+            termino = camara.termino
+            slug = camara.slug
+
+            camara_data = {
+                'nome': nome,
+                'ativo': ativo,
+                'inicio': inicio,
+                'termino': termino,
+                'slug': slug
+            }
+
+            camaras_data.append(camara_data)
+
+        kwargs['camaras_data'] = camaras_data
+
+        return super().get_context_data(**kwargs)
