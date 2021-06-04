558bb4a2e6585becc3935c9ca7088299e0f9d428
svhenrique <silviohenrique1234@hotmail.com>
2021-06-04 17:05:18 -0300

    modelos atualizados

--- votes/admin.py
+++ votes/admin.py
@@ -5,7 +5,7 @@ class ObjetosAdmin(admin.ModelAdmin):
     list_display = ('nome', 'votos')
 
 class CamaraVotoAdmin(admin.ModelAdmin):
-    list_display = ('ativo', 'nome', 'criado', 'modificado', 'inicio', 'termino')
+    list_display = ('ativo', 'nome', 'slug', 'criado', 'modificado', 'inicio', 'termino')
 
 admin.site.register(Objeto, ObjetosAdmin)
 admin.site.register(CamaraVoto, CamaraVotoAdmin)
--- votes/models.py
+++ votes/models.py
@@ -3,6 +3,9 @@ import uuid
 from django.db import models
 from stdimage.models import StdImageField
 
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+
 def get_file_path(_instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
@@ -17,12 +20,12 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
-
 class CamaraVoto(Base):
-    # ver como deixar apenas 1 camara funcionando
-    nome = models.CharField('Nome da Camara de Votos', max_length=30)
+
+    nome = models.CharField('Nome da Camara de Votos', max_length=30, unique=True)
     inicio = models.DateTimeField('Inicio')
     termino = models.DateTimeField('Termino')
+    slug = models.SlugField('Slug', max_length=50, blank=True, editable=False)
 
     def __str__(self):
         return self.nome
@@ -35,3 +38,9 @@ class Objeto(models.Model):
     imagem = StdImageField('Imagem', default="/static/img/blank-profile.png", upload_to=get_file_path,
                     variations={'thumb': {'width': 400, 'height': 400, 'crop': True}})
     votos = models.IntegerField('Votos', default=0)
+
+
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+signals.pre_save.connect(produto_pre_save, sender=CamaraVoto)
