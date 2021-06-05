40a828fee49279d89e2abc9fa26629980355ebba
svhenrique <silviohenrique1234@hotmail.com>
2021-06-04 23:02:47 -0300

    adicionado novo signal em models

--- votes/models.py
+++ votes/models.py
@@ -6,6 +6,12 @@ from stdimage.models import StdImageField
 from django.db.models import signals
 from django.template.defaultfilters import slugify
 
+from django.dispatch import receiver
+from django.core.signals import request_started
+
+import datetime
+import pytz
+
 def get_file_path(_instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
@@ -39,8 +45,26 @@ class Objeto(models.Model):
                     variations={'thumb': {'width': 400, 'height': 400, 'crop': True}})
     votos = models.IntegerField('Votos', default=0)
 
+@receiver(request_started)
+def atualizar_camaras(sender, environ, **kwargs):
+    """
+    - Serve para verificar e atualizar, a cada request, se o tempo de uma camara já se
+    encerrou, mudando seu estado de ativo (True) pra não ativo (False).
+    - Explicando sobre timezone em python e em como lidar com naive datetime e aware datetime:
+        - https://howchoo.com/g/ywi5m2vkodk/working-with-datetime-objects-and-timezones-in-python
+    """
 
+    camaras = CamaraVoto.objects.all()
+    for camara in camaras:
+
+        termino = camara.termino
+        agora = pytz.timezone("UTC").localize(datetime.datetime.now())
+
+        if termino < agora:
+            camara.ativo = False
+            camara.save()
+
+@receiver(signals.pre_save, sender=CamaraVoto)
 def produto_pre_save(signal, instance, sender, **kwargs):
     instance.slug = slugify(instance.nome)
 
-signals.pre_save.connect(produto_pre_save, sender=CamaraVoto)
